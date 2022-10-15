148aa42146eb408e3dbe49d027b7c6ef348b611c
Becky-f <backyb868@gmail.com>
2022-10-14 23:54:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,20 @@
-#include<stdio.h>
 
-int main()
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *
+ *                puzzle, followed by a new line.
+ *
+ *     * Return: Always 0.
+ */
+
+int main(void)
 
 {
 
-	char Mystr[] = "Programming is like building a multilingual puzzle \n";
-        puts(Mystr);
-       	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 
 }
